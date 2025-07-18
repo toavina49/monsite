eefb6ccf7fc148e223f466404f851715a3443718
toavina49 <toavinaanicet@gmail.com>
2025-07-18 05:57:54 +0300

    Backend Flask de tracking

--- server.py
+++ server.py
@@ -1,21 +1,11 @@
 from flask import Flask, request
-from flask_cors import CORS
-from datetime import datetime
-
 app = Flask(__name__)
-CORS(app)  # Active CORS pour accepter les requêtes cross-origin
 
 @app.route('/track', methods=['POST'])
 def track():
-    data = request.get_json()
-    ip = request.remote_addr
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_line = f"[{timestamp}] {ip} > {data}\n"
-    print(log_line)  # Affiche dans la console
-    with open("log_visiteurs.txt", "a") as f:
-        f.write(log_line)
-    return {"status": "ok"}
+    data = request.json
+    print(f"Reçu: {data}")
+    return {"status": "ok"}, 200
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
